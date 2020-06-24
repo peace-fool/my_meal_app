f9757e2574ff85f549c8ee5936f8e6677a887067
utkc137-codeverse <utkarsh.learn2020@gmail.com>
2020-06-24 22:30:09 +0530

    #

--- lib/screen/tabs_screen.dart
+++ lib/screen/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widget/main_drawer.dart.dart';
 import 'favourite_screen.dart';
 import 'categories_screen.dart';
 
@@ -27,6 +28,7 @@ class _TabsScreenState extends State<TabsScreen>{
         appBar: AppBar(
           title: Text(_pages[_selectedPageIndex]['title']),
           ),
+          drawer: MainDrawer(),
           body: _pages[_selectedPageIndex]['page'],
           bottomNavigationBar: BottomNavigationBar(
           backgroundColor: Theme.of(context).primaryColor,
